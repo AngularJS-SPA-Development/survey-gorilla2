f053d661dcc532070367dda7ffffb78bb5429382
Peter Yun <nulpulum@gmail.com>
2016-07-09 13:09:52 +0900

    fix(main): fix main page

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -1,77 +1,84 @@
-(function () {
-  'use strict';
+(function() {
+    'use strict';
 
-  angular
-    .module('surveyGorillaApp')
-    .controller('MainCtrl', MainCtrl);
+    angular
+        .module('surveyGorillaApp')
+        .controller('MainCtrl', MainCtrl);
 
-  /* @ngInject */
-  function MainCtrl($scope, group, modal, logger, sgAlert) {
-    var vm = this;
-    vm.createGroup = createGroup;
-    vm.showGroupDetail = showGroupDetail;
-    _init(); 
-    
-    function _init() {
-      // change it to $watch
-      //_groups(true);
-      //_groups(false);
-    }
+    /* @ngInject */
+    function MainCtrl($scope, group, modal, logger, sgAlert) {
+        var vm = this;
+        vm.createGroup = createGroup;
+        vm.showGroupDetail = showGroupDetail;
+        _init();
 
-    function _groups(isMyGroup, params) {
-      group 
-        .getGroups(isMyGroup, params)
-        .then(function(response) {
-          logger.info('group list: ', response.data);
-          if(isMyGroup) {
-            vm.myGroups = response.data;
-          } else {
-            vm.otherGroups = response.data;
-          }
+        function _init() {
+            // change it to $watch
+            //_groups(true);
+            //_groups(false);
+        }
+
+        function _groups(isMyGroup, params) {
+            group
+                .getGroups(isMyGroup, params)
+                .then(function(response) {
+                    logger.info('group list: ', response.data);
+                    if (isMyGroup) {
+                        vm.myGroups = response.data;
+                    } else {
+                        vm.otherGroups = response.data;
+                    }
+                });
+        }
+
+        function createGroup() {
+            modal
+                .open('sm', 'create-group.html', 'CreateGroupCtrl')
+                .then(function(result) {
+                    logger.info('create group result: ', result);
+                    vm.myGroups.unshift(result);
+                }, function(error) {});
+        }
+
+        function showGroupDetail(group, type) {
+            modal
+                .open('', 'read-group.html', 'ReadGroupCtrl', {
+                    group: group,
+                    type: type
+                })
+                .then(function(result) {
+                    if (result && result.memberEnroll) {
+                        delete result.memberEnroll;
+                        vm.otherGroups = _.without(vm.otherGroups, result);
+                        vm.myGroups.unshift(result);
+                    }
+
+                    if (result && result.memberLeave) {
+                        delete result.memberLeave;
+                        vm.myGroups = _.without(vm.myGroups, result);
+                        vm.otherGroups.unshift(result);
+                    }
+
+                    logger.info('read group result: ', result);
+                }, function(error) {});
+        }
+
+        $scope.$watch(function() {
+            return vm.myGroupName;
+        }, function(newVal, oldVal) {
+            _groups(true, {
+                name: vm.myGroupName
+            });
         });
+
+        $scope.$watch(function() {
+            return vm.otherGroupName;
+        }, function(newVal, oldVal) {
+            _groups(false, {
+                name: vm.otherGroupName
+            });
+        });
+
     }
-
-    function createGroup() {
-      modal
-        .open('sm', 'create-group.html', 'CreateGroupCtrl')
-        .then(function(result){
-          logger.info('create group result: ', result);
-          vm.myGroups.unshift(result);
-        }, function(error) {});
-    }
-
-    function showGroupDetail(group, type) {
-      modal
-        .open('', 'read-group.html', 'ReadGroupCtrl', {group: group, type: type})
-        .then(function(result){
-          if(result && result.memberEnroll) {
-            delete result.memberEnroll;
-            vm.otherGroups = _.without(vm.otherGroups, result);
-            vm.myGroups.unshift(result);
-          }
-
-          if(result && result.memberLeave) {
-            delete result.memberLeave;
-            vm.myGroups = _.without(vm.myGroups, result);
-            vm.otherGroups.unshift(result);
-          }
-
-          logger.info('read group result: ', result);
-        }, function(error) {});
-    }
-
-    $scope.$watch(function() { 
-      return vm.myGroupName; 
-    }, function(newVal, oldVal) {
-      _groups(true, {name: vm.myGroupName});
-    });
-
-    $scope.$watch(function() { 
-      return this.otherGroupName; 
-    }, function(newVal, oldVal) {
-      _groups(false, {name: vm.otherGroupName});
-    });
-    
-  }
 
 })();
--- client/components/base/modal/modal.service.js
+++ client/components/base/modal/modal.service.js
@@ -1,38 +1,38 @@
 (function() {
-  'use strict';
+    'use strict';
 
-  angular
-    .module('sg.base')
-    .service('modal', modal);
+    angular
+        .module('sg.base')
+        .service('modal', modal);
 
-  /* @ngInject */
-  function modal($modal, $q) {
-    this.open = open;
+    /* @ngInject */
+    function modal($uibModal, $q) {
+        this.open = open;
 
-    /**
-     * size : '', 'lg', 'sm'
-     */
-    function open(size, templateId, controller, params) {
-      var modalInstance = $modal.open({
-        templateUrl: templateId, //'myModalContent.html',
-        controller: controller, //'ModalInstanceCtrl',
-        size: size,
-        resolve: {
-          params: function () {
-            return params;
-          }
+        /**
+         * size : '', 'lg', 'sm'
+         */
+        function open(size, templateId, controller, params) {
+            var modalInstance = $uibModal.open({
+                templateUrl: templateId, //'myModalContent.html',
+                controller: controller, //'ModalInstanceCtrl',
+                size: size,
+                resolve: {
+                    params: function() {
+                        return params;
+                    }
+                }
+            });
+
+            var deferred = $q.defer();
+            modalInstance.result.then(function(result) {
+                deferred.resolve(result);
+            }, function(error) {
+                deferred.reject(error);
+            });
+
+            return deferred.promise;
         }
-      });
-
-      var deferred = $q.defer();
-      modalInstance.result.then(function (result) {
-        deferred.resolve(result);
-      }, function (error) {
-        deferred.reject(error);
-      });
-
-      return deferred.promise;
     }
-  }
 
 })();
